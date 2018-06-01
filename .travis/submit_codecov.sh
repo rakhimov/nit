0440421a015aae932562f0bc51b841a8c5ec28f5
rakhimov <ol.rakhimov@gmail.com>
2018-06-01 14:09:29 -0700

    Rename pokerstove -> nit in Codecov submission
    Issue #3

--- .travis/submit_codecov.sh
+++ .travis/submit_codecov.sh
@@ -3,11 +3,11 @@
 set -ev
 
 # Submit coverage of C++
-PENUM_COV_DIR="build/src/pokerstove/penum/CMakeFiles/penum.dir/"  # TODO: Enable
-PENUM_TESTS_COV_DIR="build/tests/pokerstove/penum/CMakeFiles/penum_tests.dir/"
-PEVAL_COV_DIR="build/src/pokerstove/peval/CMakeFiles/peval.dir/"
-PEVAL_TESTS_COV_DIR="build/tests/pokerstove/peval/CMakeFiles/peval_tests.dir/"
-UTIL_TESTS_COV_DIR="build/tests/pokerstove/util/CMakeFiles/util_tests.dir/"
+PENUM_COV_DIR="build/src/nit/penum/CMakeFiles/penum.dir/"  # TODO: Enable
+PENUM_TESTS_COV_DIR="build/tests/nit/penum/CMakeFiles/penum_tests.dir/"
+PEVAL_COV_DIR="build/src/nit/peval/CMakeFiles/peval.dir/"
+PEVAL_TESTS_COV_DIR="build/tests/nit/peval/CMakeFiles/peval_tests.dir/"
+UTIL_TESTS_COV_DIR="build/tests/nit/util/CMakeFiles/util_tests.dir/"
 TRACE_FILE="coverage.info"
 lcov --no-compat-libtool \
   --directory $PENUM_TESTS_COV_DIR \
