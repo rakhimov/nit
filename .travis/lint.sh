a1d3693700830d660f12b04bc47086588a95545d
rakhimov <ol.rakhimov@gmail.com>
2018-06-02 22:46:55 -0700

    Add linting stage to Travis-CI [skip appveyor]

---
+++ .travis/lint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -ev
+set -o pipefail
+
+CLANG_FORMAT="./run-clang-format/run-clang-format.py --clang-format-executable clang-format-${CLANG_VERSION}"
+
+${CLANG_FORMAT} -r src/
+${CLANG_FORMAT} -r tests/
