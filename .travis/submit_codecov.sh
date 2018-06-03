e17d6ad50cf583a61ad07ca3aa9f9a0f8d8c1106
rakhimov <ol.rakhimov@gmail.com>
2018-06-03 01:43:35 -0700

    Update codecov config after p{enum,eval} rename
    - Exclude cli tools from coverage.
    - lcov processes build directory recursively.

--- .travis/submit_codecov.sh
+++ .travis/submit_codecov.sh
@@ -3,17 +3,8 @@
 set -ev
 
 # Submit coverage of C++
-PENUM_COV_DIR="build/src/nit/penum/CMakeFiles/penum.dir/"  # TODO: Enable
-PENUM_TESTS_COV_DIR="build/tests/nit/penum/CMakeFiles/penum_tests.dir/"
-PEVAL_COV_DIR="build/src/nit/peval/CMakeFiles/peval.dir/"
-PEVAL_TESTS_COV_DIR="build/tests/nit/peval/CMakeFiles/peval_tests.dir/"
-UTIL_TESTS_COV_DIR="build/tests/nit/util/CMakeFiles/util_tests.dir/"
 TRACE_FILE="coverage.info"
-lcov --no-compat-libtool \
-  --directory $PENUM_TESTS_COV_DIR \
-  --directory $PEVAL_COV_DIR \
-  --directory $PEVAL_TESTS_COV_DIR \
-  --directory $UTIL_TESTS_COV_DIR \
+lcov --no-compat-libtool --directory build \
   -c --rc lcov_branch_coverage=1 -o $TRACE_FILE -q
 lcov --extract $TRACE_FILE '*/nit/src/*' -o $TRACE_FILE
 codecov > /dev/null
